36da0c3472331c9845029d5b1e54dce4b2d45333
HarukiIsono <haruki.isono861@gmail.com>
2025-12-07 19:14:08 +0900

    fix

--- launch/camina.launch.py
+++ launch/camina.launch.py
@@ -36,41 +36,6 @@ def generate_launch_description():
             arguments=[urdf]
         ),
 
-        # Dual camera sync republisher
-        # Node(
-        #     package='cibo',
-        #     executable='dual_camera_sync_node',
-        #     name='dual_camera_sync_node',
-        #     output='screen',
-        #     parameters=[{'queue_size': 20, 'slop_sec': 0.08, 'restamp': False}],
-        # ),
-
-        # front_camera_depth_node -> /sync/camera_01/** を購読
-        Node(
-            package='cibo',
-            executable='front_camera_depth_node',
-            name='front_camera_node',
-            output='screen',
-            # remappings=[
-            #     ('/camera_01/color/image_raw', '/sync/camera_01/color/image_raw'),
-            #     ('/camera_01/depth/image_raw', '/sync/camera_01/depth/image_raw'),
-            #     ('/camera_01/depth/camera_info', '/sync/camera_01/depth/camera_info'),
-            # ]
-        ),
-
-        # top_camera_depth_node -> /sync/camera_02/** を購読
-        Node(
-            package='cibo',
-            executable='top_camera_depth_node',
-            name='top_camera_node',
-            output='screen',
-            # remappings=[
-            #     ('/camera_02/color/image_raw', '/sync/camera_02/color/image_raw'),
-            #     ('/camera_02/depth/image_raw', '/sync/camera_02/depth/image_raw'),
-            #     ('/camera_02/depth/camera_info', '/sync/camera_02/depth/camera_info'),
-            # ]
-        ),
-
         Node(
             package='rviz2', 
             executable='rviz2',
